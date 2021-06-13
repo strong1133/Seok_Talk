816355ec5a88ce9b5b202624e418c8e8f9457531
[strong1133] <[strong1133@naver.com]>
2021-06-13 14:37:06 +0900

    stomp

--- Backend/src/main/java/com/seok_talk/seok_talk/config/WebSockConfig.java
+++ Backend/src/main/java/com/seok_talk/seok_talk/config/WebSockConfig.java
@@ -1,9 +1,11 @@
 package com.seok_talk.seok_talk.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 
 
 @Configuration
